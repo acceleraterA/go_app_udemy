c0f4a905ab628062bb6cf2851806d6f804dbdff5
Mengzhu <mzga@ucdavis.edu>
2023-09-21 13:53:04 -0700

    update migrations

---
+++ migrations/20230921204112_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES('JC', 'Tan', 'admin@admin.com', '$2a$12$TMO6NGx5xSPqgfFAt9ivLexhjKBf6/E6hwZKnonFhbOpYSzd3Skn6', 3, '2023-05-08 00:00:00.000', '2023-05-08 00:00:00.000');
